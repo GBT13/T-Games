b83ce5832549ff7db2a9601eb0689a3d829bbec7
Testman3 <testman3@hotmail.com>
2018-04-23 22:12:00 +0200

    Added more seeding stuff
    In preparation for assessment

--- database/seeds/GamesTableSeeder.php
+++ database/seeds/GamesTableSeeder.php
@@ -11,10 +11,10 @@ class GamesTableSeeder extends Seeder {
      */
     public function run() {
         try {
-            $json = File::get("database/gamedata/top100Games.json");
+//            $json = File::get("database/gamedata/top100Games.json");
 
 //            This one might be slight overkill
-//            $json = File::get("database/gamedata/allsteamgames.json");
+            $json = File::get("database/gamedata/allsteamgames.json");
         } catch (\Illuminate\Contracts\Filesystem\FileNotFoundException $e) {
 
         }
@@ -29,6 +29,121 @@ class GamesTableSeeder extends Seeder {
                 'publisher' => $game->publisher
             ));
         }
+
+        Game::create([
+            'name'=>'World of Warcraft',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Starcraft 2',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Starcraft',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Overwatch',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Hearthstone',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Diablo 3',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Heroes of the Storm',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'League of Legends',
+            'developer'=>'Riot Games',
+        ]);
+
+        Game::create([
+            'name'=>'Fortnite',
+            'developer'=>'Epic Games',
+        ]);
+
+        Game::create([
+            'name'=>'World of Warcraft',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Starcraft 2',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Starcraft',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Overwatch',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Hearthstone',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Diablo 3',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'Heroes of the Storm',
+            'developer'=>'Blizzard Entertainment',
+        ]);
+
+        Game::create([
+            'name'=>'League of Legends',
+            'developer'=>'Riot Games',
+        ]);
+
+        Game::create([
+            'name'=>'Battlefield 1',
+            'developer'=>'DICE',
+            'publisher'=>'EA Games',
+        ]);
+
+        Game::create([
+            'name'=>'Battlefield 4',
+            'developer'=>'DICE',
+            'publisher'=>'EA Games',
+        ]);
+
+        Game::create([
+            'name'=>'The Sims 4',
+            'developer'=>'Maxis',
+            'publisher'=>'EA Games',
+        ]);
+
+        Game::create([
+            'name'=>'The Sims 3',
+            'developer'=>'Maxis',
+            'publisher'=>'EA Games',
+        ]);
+
+        Game::create([
+            'name'=>'Simcity',
+            'developer'=>'Maxis',
+            'publisher'=>'EA Games',
+        ]);
 
     }
 }
